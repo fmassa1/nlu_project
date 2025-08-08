360af1ecfabc7e5ed69b8fc94b80932f8a09408a
Fredric Massa <rickmass2020@gmail.com>
2025-08-08 16:30:58 -0500

    set up routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
+
 
 import Navbar from './components/navbar';
 import Footer from './components/footer';
+
+import HomePage from './pages/home';
+import Flavors from './pages/flavors';
+import NotFound from './pages/404'
+
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +20,14 @@ function App() {
 
       <Navbar />
 
-      <h1>Vite + React</h1>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/capabilities" element={<h1>Capabilities</h1>} />
+        <Route path="/flavors" element={<Flavors />} />
+        <Route path="/contact" element={<h1>contact</h1>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+
       <Footer />
     </>
   )
