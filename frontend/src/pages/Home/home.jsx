0c8a8b36eeacf53d4ba3b8bca02be0daf2680823
Fredric Massa <rickmass2020@gmail.com>
2025-08-09 18:51:27 -0500

    test for deployment

--- frontend/src/pages/Home/home.jsx
+++ frontend/src/pages/Home/home.jsx
@@ -9,7 +9,15 @@ function HomePage() {
             <Slider />
             <div className="main-content-wrapper" style={{ display: 'flex', gap: '2rem' }}>
                 <div className="content-left">
-                    <h1>HomePage</h1>
+                    <h1>Design. Manufacture. Deliver</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+
+                    </p>
                 </div>
                 <div className="quote-container-right">
                     <QuoteForm />
